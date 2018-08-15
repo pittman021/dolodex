28b0c3d24817468fbb953c5a87b46253503d8b6c
Tim Pittman <tim.pittman021@gmail.com>
2018-08-14 22:43:31 -0500

    conditional navbars & users birthdays

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
     @contacts = current_user.contacts.all
     @lists = current_user.lists.all
 
-    @birthdays = Contact.where("birthday_month <= ? AND birthday_month >= ?", Date.today.month, Date.today.next.month).order(:birthday_day)
+    @birthdays = current_user.contacts.where("birthday_month <= ? AND birthday_month >= ?", Date.today.month, Date.today.next.month).order(:birthday_day)
   end
 
   def upload
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def flash_class(level)
+    case level
+        when :notice then "notice is-primary box cta"
+        when :success then "alert alert-success box cta"
+        when :error then "alert alert-error"
+        when :alert then "alert alert-error"
+    end
+end
 end
