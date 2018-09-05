3fb3da08e40633be6414db105ee93d134af77397
Tim Pittman <tim.pittman021@gmail.com>
2018-09-04 22:25:30 -0500

    fix dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
 
 	def index
+		@contacts_count = current_user.contacts.count
+		@lists_count = current_user.lists.count
+
 		@lists = current_user.lists.all
     @birthdays = current_user.contacts.where("birthday_month >= ? AND birthday_month <= ?", Time.now.month, (Time.now + 1.month).month).order(:birthday_month)
 end
