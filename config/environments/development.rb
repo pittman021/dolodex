e07e3d813036f16a5c78997fc4504bde72892f22
Tim Pittman <tim.pittman021@gmail.com>
2018-09-09 23:10:03 -0500

    add event vars to dev.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.smtp_settings = {
-    :user_name => '',
-    :password => '',
+    :user_name => 'ENV['SENDGRID_USERNAME']',
+    :password => 'ENV['SENDGRID_PASSWORD']',
     :domain => 'localhost:3000',
     :address => 'smtp.sendgrid.net',
     :port => 587,
