96292d273fb1d6043be8747e8a8d99911ade660a
Tim Pittman <tim.pittman021@gmail.com>
2018-09-12 11:42:15 -0500

    fix page styles & contact delete route

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,7 @@ class Contact < ApplicationRecord
 	validates_presence_of :first_name, :last_name
 
 	belongs_to :user
-	has_many :gifts
+	has_many :gifts, dependent: :destroy
 	has_and_belongs_to_many :lists
 
 	accepts_nested_attributes_for :gifts, :allow_destroy => true
